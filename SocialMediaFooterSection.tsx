dd8c0fad4244ccb2359563407f990cee49c65b0d
Queen Cobra <queeenqobra@gmail.com>
2026-03-27 15:36:06 +0300

    Update social media links in SocialMediaFooterSection.tsx

---
+++ SocialMediaFooterSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SocialMediaFooterSection = () => {
+    return (
+        <footer>
+            <h2>Follow Us</h2>
+            <ul>
+                <li><a href="https://t.me/cultimm">Telegram</a></li>
+                <li><a href="https://discord.gg/Vu8kNs39PX">Discord</a></li>
+                <li><a href="https://www.youtube.com/@immortalitysekta">YouTube</a></li>
+                <li><a href="https://www.tiktok.com/@immortality_dt2">TikTok</a></li>
+                <li><a href="https://t.me/informationimm">ИНФОРМАЦИЯ</a></li>
+                <li><a href="https://t.me/reglamentbyimmortality">РЕГЛАМЕНТ</a></li>
+            </ul>
+        </footer>
+    );
+};
+
+export default SocialMediaFooterSection;
